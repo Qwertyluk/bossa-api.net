f43b9fc44f14e19c815d52e3120a7825444992bc
Jan <krzyzakjan@poczta.onet.pl>
2023-03-18 11:00:27 +0100

    Add Console application for testing

---
+++ Examples/ConsoleAppTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using pjank.BossaAPI.src;
+
+namespace ConsoleAppTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bossa = new BossaWrapper();
+            bossa.ConnectNOL3();
+
+            var account = bossa.Accounts["00-55-048082"];
+            var instrument = bossa.Instruments["TAURONPE"];
+
+            var order = instrument.Buy(1.9m, 1, new DateTime(2023, 3, 20));
+
+            order.Cancel();
+
+            bossa.Disconnect();
+        }
+    }
+}
