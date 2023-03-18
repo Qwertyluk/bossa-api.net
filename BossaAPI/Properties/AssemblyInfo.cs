36b7f046fc581d99007924bb7fa2ce65b9606185
Jan <krzyzakjan@poczta.onet.pl>
2023-03-18 10:41:53 +0100

    Set BossaAPI COM visibility to true

--- BossaAPI/Properties/AssemblyInfo.cs
+++ BossaAPI/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a65366d7-65d5-4211-b9b9-def50aba3523")]
